53ddec1910a3720a0a0aac1e49e83e7dfa22c54e
Alec Barnard <alecbarnard@live.com>
2022-01-29 14:52:20 -0800

    sign in working

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -2,8 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
   return (
@@ -28,9 +27,18 @@ export default function Home() {
           <p className="my-2 text-center">Create your profile to start desigining your own quizzes for your candidates.</p>
           <a href="signup" className="text-white font-semibold bg-indigo-600 w-28 m-2 py-2 px-6 rounded-md mx-auto">Sign Up</a>
         </div>
-    
+
+
+        <div className="my-20 grid grid-rows-3 text-center ">
+          <h2 className="text-center text-2xl text-indigo-900">Get Started</h2>
+          <p className="my-2 text-center">Create your profile to start desigining your own quizzes for your candidates.</p>
+          <a onClick={() => signIn('cognito', {
+            callbackUrl: `${window.location.origin}/profile`
+          })} className="text-white font-semibold bg-indigo-600 w-28 m-2 py-2 px-6 rounded-md mx-auto">Sign In</a>
+        </div>
+
       </main>
-      
+
     </div>
   )
 }
