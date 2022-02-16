734d0522f6a22253f05c6f62a3f26c955959c08b
Joel Chenoweth <jchenwth@gmail.com>
2022-02-16 00:04:07 -0600

    lamda_function.py

--- lambdas/quiz-invite-lambda/lambda_function.py
+++ lambdas/quiz-invite-lambda/lambda_function.py
@@ -1,32 +1,33 @@
 import boto3
 import json
+import mysql.connector
 
 
 def lambda_handler(event, context):
     """
     Creates a quiz invite with provided details from event
     """
-
-    # Setup cognito client
-    client = boto3.client('cognito-idp')
-
     # Get user properties from event
     userId, quizId, email = event["userId"], event["quizId"], event["email"],
 
-    try:
-        response = client.admin_create_user(
-            UserPoolId="us-east-1_mi0MGd08p",
-            Username=email,
-            UserAttributes=[
-                {"Name": "userId", "Value": f"{userId}"},
-                {"Name": "quizId", "Value": f"{quizId}"},
-                {"Name": "email", "Value": email},
-                {"Name": "email_verified", "Value": "true"}
-            ],
-            DesiredDeliveryMediums=['EMAIL']
-        )
+    mydb = mysql.connector.connect(
+        host="sd1e7h3lcrgky97.ctnnda3nrolp.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="password1",
+        database="Software_Quiz"
+    )
 
-        return json.dumps(response)
+    mycursor = mydb.cursor()
 
-    except Exception:
-        return None
+    sql = "INSERT INTO Quiz_Invite ( quiz_id, email) VALUES ( %s, %s)"
+    val = (quizId, email)
+    mycursor.execute(sql, val)
+    #mycursor.execute("INSERT INTO Quiz_Invite ( quiz_id, email) VALUES ( '123', 'test@gmail.com')")
+
+    mydb.commit()
+
+    # Setup cognito client
+    # client = boto3.client('cognito-idp')
+
+    return mycursor.rowcount
+
