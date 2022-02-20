add07caa10cf9eebd3d4ab8a425808399955090b
Alec Barnard <alecbarnard@live.com>
2022-02-19 19:22:59 -0800

    sql insert

--- database/create.sql
+++ database/create.sql
@@ -3,8 +3,12 @@ CREATE DATABASE Software_Quiz;
 USE Software_Quiz;
 
 CREATE TABLE Quiz_Invite (
-	invite_id VARCHAR(36) default (uuid()) NOT NULL PRIMARY KEY,
+	id VARCHAR(36) default (uuid()) NOT NULL PRIMARY KEY,
     quiz_id VARCHAR(36) NOT NULL,
     email VARCHAR(320) NOT NULL
 )
 
+CREATE TABLE Quiz (
+    id VARCHAR(36) default (uuid()) NOT NULL PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL
+)
--- lambdas/create-quiz-lambda/lambda_function.py
+++ lambdas/create-quiz-lambda/lambda_function.py
@@ -1,18 +1,48 @@
-import boto3
+#import boto3
 import json
+import mysql.connector
 
 def lambda_handler(event, context):
     """
     Creates a new quiz object in documentDB and RDS
     """
 
-    # Verify user ??
+    # Db connection
+    db = mysql.connector.connect(
+        host="sd1e7h3lcrgky97.ctnnda3nrolp.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="password1",
+        database="software_quiz",
+    )
 
-    # Create RDS entry to get quizID
-    # QuizId, UserId
+    # Setup cursor
+    cursor = db.cursor()
+
+    cursor.execute("SELECT UUID()")
+    uuid = cursor[0]
+    cursor.execute("INSERT INTO Quiz (id, user_id) VALUES (%s, %s)", (uuid, "1234567",))
+
+    # Save data and close connections
+    db.commit()
+    cursor.close()
+    db.close()
 
 
-    # Create dynamoDB instance
-    # QuizId, Name, Questions, Answer
+    # Upload document to s3
+
+    #s3_client = boto3.client('s3')
+
+    # file = json.dumps(quiz)
+
+    # response = s3_client.upload_file(file_name, bucket, object_name)
+
 
     return
+
+
+
+if __name__ == "__main__":
+
+    event = {}
+
+    lambda_handler(event, {})
