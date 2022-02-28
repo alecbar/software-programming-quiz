785b31f1cc8c89102401cfff66b4ece0428d1a34
Alec Barnard <alecbarnard@live.com>
2022-02-27 17:29:05 -0800

    fix negative timer

--- next-app/pages/quiz/take/[id].js
+++ next-app/pages/quiz/take/[id].js
@@ -1,26 +1,30 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 
-export default function TakeQuiz(){
+export default function TakeQuiz() {
 
     // Time in seconds
-    const [timer, setTimer] = useState(65)
+    const [timer, setTimer] = useState(5)
     const [timerString, setTimerString] = useState(new Date(timer * 1000).toISOString().substr(11, 8))
 
     const decreaseTimer = () => {
-        setTimer(timer-1)
+        setTimer(timer - 1)
     }
 
     // Every time our timer changes, we want to decrease the time again in 1 second
-    useEffect(()=>{
-        // Decrease the time in 1 second
-        // Retain ID so we can cancel the itnerval and only decrease 1 time
-        const intervalId = setInterval(decreaseTimer, 1000)
-        return () => clearInterval(intervalId)
+    useEffect(() => {
+
+        if (timer > 0) {
+            // Decrease the time in 1 second
+            // Retain ID so we can cancel the itnerval and only decrease 1 time
+            const intervalId = setInterval(decreaseTimer, 1000)
+            return () => clearInterval(intervalId)
+        }
+
     }, [timer])
 
-    //
-    useEffect(()=>{
+    // Update time string when timer changes
+    useEffect(() => {
         setTimerString(new Date(timer * 1000).toISOString().substr(11, 8))
     }, [timer])
 
